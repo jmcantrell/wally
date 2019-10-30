983b752bdcc0aae5bbc90613a4c569fbfa23c11d
Jeremy Cantrell <jmcantrell@gmail.com>
2019-10-30 00:12:00 -0500

    Change formatting to meet python style recommendations

--- setup.py
+++ setup.py
@@ -3,39 +3,48 @@
 from setuptools import setup
 
 setup(
-        name='Wally',
-        version='0.7.3',
-        description='Tool for managing desktop backgrounds.',
-        author='Jeremy Cantrell',
-        author_email='jmcantrell@gmail.com',
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Environment :: X11 Applications :: Gnome',
-            'Intended Audience :: End Users/Desktop',
-            'License :: OSI Approved :: BSD License',
-            'Natural Language :: English',
-            'Operating System :: POSIX :: Linux',
-            'Programming Language :: Python',
-            'Topic :: Desktop Environment :: Gnome',
-            ],
-        install_requires=[
-            'ScriptUtils',
-            'GNOMEUtils',
-            'GTKUtils',
-            'ImageUtils',
-            'PathUtils',
-            ],
-        entry_points={
-            'console_scripts': [
-                'wally=wally.__main__:main',
-                ],
-            },
-        packages=[
-            'wally',
-            ],
-        package_data={
-            'wally': [
-                'icons/*.svg',
-                ],
-            },
-        )
+
+    name='wally',
+    version='0.7.3',
+
+    description='Tool for managing desktop backgrounds.',
+
+    author='Jeremy Cantrell',
+    author_email='jmcantrell@gmail.com',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: X11 Applications :: Gnome',
+        'Intended Audience :: End Users/Desktop',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Desktop Environment :: Gnome',
+    ],
+
+    install_requires=[
+        'scriptutils',
+        'gnomeutils',
+        'gtkutils',
+        'imageutils',
+        'pathutils',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'wally=wally.__main__:main',
+        ],
+    },
+
+    packages=[
+        'wally',
+    ],
+
+    package_data={
+        'wally': [
+            'icons/*.svg',
+        ],
+    },
+
+)
